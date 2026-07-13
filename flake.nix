cf449740b74900fb130cac9866ec1e3953b97b37
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-07-14 00:14:00 +0900

    initial nixos testing

--- flake.nix
+++ flake.nix
@@ -18,8 +18,86 @@
       ];
 
       perSystem =
-        { pkgs, system, ... }:
         {
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        let
+          linux-tpm-fido2 = pkgs.rustPlatform.buildRustPackage {
+            pname = "linux-tpm-fido2";
+            version = "0.1.0";
+            src = ./.;
+
+            _structuredAttrs = true;
+            strictDeps = true;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            env.SQLX_OFFLINE = "true";
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
+
+            buildInputs = with pkgs; [
+              tpm2-tss
+            ];
+          };
+
+          linux-tpm-fido2-smoke = pkgs.writeShellApplication {
+            name = "linux-tpm-fido2-smoke";
+            runtimeInputs = with pkgs; [
+              coreutils
+              (python3.withPackages (ps: [
+                ps.cbor2
+                ps.fido2
+              ]))
+            ];
+            text = lib.readFile ./fido-smoke.sh;
+          };
+
+          linux-tpm-fido2-nixos-test = pkgs.testers.runNixOSTest {
+            name = "linux-tpm-fido2";
+
+            nodes.machine =
+              { ... }:
+              {
+                environment.systemPackages = [
+                  linux-tpm-fido2
+                  pkgs.kmod
+                  pkgs.swtpm
+                ];
+
+                virtualisation.memorySize = 1536;
+              };
+
+            testScript = ''
+              start_all()
+
+              machine.succeed("mkdir -p /tmp/linux-tpm-fido2-smoke/swtpm /tmp/linux-tpm-fido2-smoke/store")
+              machine.succeed("modprobe cuse")
+              machine.succeed("nohup swtpm cuse --name=tpm0 --tpm2 --tpmstate dir=/tmp/linux-tpm-fido2-smoke/swtpm --flags startup-clear >/tmp/linux-tpm-fido2-smoke/swtpm.log 2>&1 &")
+              machine.wait_until_succeeds("test -e /dev/tpm0")
+
+              machine.succeed("nohup sh -c 'export RUST_LOG=debug; yes y | ${linux-tpm-fido2}/bin/linux-tpm-fido2 --tpm-path /dev/tpm0 --store-dir /tmp/linux-tpm-fido2-smoke/store' >/tmp/linux-tpm-fido2-smoke/daemon.log 2>&1 & echo $! >/tmp/linux-tpm-fido2-smoke/daemon.pid")
+              machine.wait_until_succeeds("sh -c 'set -- /dev/hidraw*; [ $# -ge 2 ]'")
+
+              machine.succeed("sh -euc 'WORKDIR=/tmp/linux-tpm-fido2-smoke RP_ID=login.example.test ${linux-tpm-fido2-smoke}/bin/linux-tpm-fido2-smoke register || { cat /tmp/linux-tpm-fido2-smoke/daemon.log; exit 1; }'")
+              machine.succeed("sh -euc 'WORKDIR=/tmp/linux-tpm-fido2-smoke RP_ID=login.example.test ${linux-tpm-fido2-smoke}/bin/linux-tpm-fido2-smoke assert || { cat /tmp/linux-tpm-fido2-smoke/daemon.log; exit 1; }'")
+
+              machine.succeed("kill $(cat /tmp/linux-tpm-fido2-smoke/daemon.pid)")
+              machine.wait_until_succeeds("! kill -0 $(cat /tmp/linux-tpm-fido2-smoke/daemon.pid)")
+            '';
+          };
+        in
+        {
+          packages.default = linux-tpm-fido2;
+
+          checks.nixos = linux-tpm-fido2-nixos-test;
+
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             overlays = [
